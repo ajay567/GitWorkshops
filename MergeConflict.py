cb65d1e957fb7cbdb0de72f8604abc2a8be4707a
ajay567 <ajay99@vt.edu>
2018-09-26 19:42:07 -0400

    Changed

--- MergeConflict.py
+++ MergeConflict.py
@@ -3,5 +3,5 @@
 
 # Each person should change to following line of code:
 
-print("This was initially just a simple sentence, but now I made it look like code. It doesn't actually matter, but it'll look more familiar this way");
+print("Changed This was initially just a simple sentence, but now I made it look like code. It doesn't actually matter, but it'll look more familiar this way");
 print("Hello world, my name is <NAME_HERE> and my favorite programming language is <LANGUAGE_HERE> ");
